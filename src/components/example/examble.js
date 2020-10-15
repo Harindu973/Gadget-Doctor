286f95ececffcb88abf73e42177b11a22c3b59b9
Harindu Lakshan <mbhlakshan@students.nsbm.lk>
2020-10-15 09:10:41 +0530

    project updated

---
+++ src/components/example/examble.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+//import "./newsletter.css";
+
+class Newsletter extends Component {
+    render(){
+        return(
+            <div>
+
+            </div>
+        );
+    }
+}
+export default Newsletter;
