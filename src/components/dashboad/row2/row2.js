7d9cbaff3af0d661916172ce1adce9b3f0e62b6e
Harindu Lakshan <harindulakshankob@gmail.com>
2020-10-26 12:57:38 +0530

    Ui updated in Row2

---
+++ src/components/dashboad/row2/row2.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import './row2.css';
+
+class Raw2 extends Component {
+    render() {
+        return (
+            <div className="row">
+                <div className="col-md-4">
+                    <div className="card card-chart">
+                        <div className="card-header card-header-success">
+                           
+                        </div>
+                        <div className="card-body">
+                            <p className="card-category">
+                            <h4 className="card-title"><b>Update Mileage Manually</b></h4>
+                                <input type="number" class="form-control" id="inputPassword2" placeholder="Enter Your Currunt Mileage" min="0" max="1000000"></input>
+                            </p>
+                        </div>
+                        <div className="">
+                            <div className="stats">
+                             <button type="button" class="btn btn-link">Update</button>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-4">
+                    <div className="card card-chart">
+                        <div className="card-header card-header-warning">
+                            <div className="ct-chart" id="websiteViewsChart" />
+                        </div>
+                        <div className="card-body">
+                            <h4 className="card-title">Email Subscriptions</h4>
+                            <p className="card-category">Last Campaign Performance</p>
+                        </div>
+                        <div className="card-footer">
+                            <div className="stats">
+                                <i className="material-icons">access_time</i> campaign sent 2 days ago
+                        </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-4">
+                    <div className="card card-chart">
+                        <div className="card-header card-header-danger">
+                            <div className="ct-chart" id="completedTasksChart" />
+                        </div>
+                        <div className="card-body">
+                            <h4 className="card-title">Completed Tasks</h4>
+                            <p className="card-category">Last Campaign Performance</p>
+                        </div>
+                        <div className="card-footer">
+                            <div className="stats">
+                                <i className="material-icons">access_time</i> campaign sent 2 days ago
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+export default Raw2;
