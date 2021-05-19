2d2520fc08f3fe38254e6e64982236eb0e40570e
Harindu973 <harindulakshankob@gmail.com>
2021-05-19 15:36:33 +0530

    Expert row tag filters

--- src/components/expert/expertrows.js
+++ src/components/expert/expertrows.js
@@ -14,19 +14,28 @@ class Expert extends Component {
         const url = "https://expert.gadgetdoc.tech/ghost/api/v3/content/posts/?key=393518c903a40dad5503c49e49&include=authors,tags";
         const response = await fetch(url);
         const data = await response.json();
-        console.log("hjvadyuewy     " + data);
-
-
+       
+        
         var c;
-        for (c = 0; c < 3; ++c) {
-            this.setState({ post: data, loading: false });
-            console.log({ post: data, loading: false });
-            document.getElementById("e-slug_" + c).innerHTML = this.state.post.posts[c].primary_author.name;
-            document.getElementById("e-link_" + c).href = this.state.post.posts[c].url;
-            document.getElementById("e-content_" + c).innerHTML = this.state.post.posts[c].title;
-            console.log("ID Is = " + this.state.post.posts[0].title);
+        var d;
+        for (c = 0; c < data.posts.length; c++) {
+            for (d = 0; d < data.posts[c].tags.length; d++) {
+
+        if(data.posts[c].tags[d].name == "Grace"){
+
+                this.setState({ post: data, loading: false });
+                //console.log(this.state.post.posts[0].tags[1].name);
+               // console.log({ post: data, loading: false });
+                document.getElementById("e-slug_" + c).innerHTML = this.state.post.posts[c].primary_author.name;
+                document.getElementById("e-link_" + c).href = this.state.post.posts[c].url;
+                document.getElementById("e-content_" + c).innerHTML = this.state.post.posts[c].title;
 
         }
+       console.log(data.posts[0].tags.length);
+       
+        }
+
+    }
 
         //console.log(this.state);
 
